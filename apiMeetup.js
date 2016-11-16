16e808eda647c0b99893ef3888f97cf3c792cdde
Aakash <ahanda205@gmail.com>
2016-11-16 09:56:27 +0530

    Update apiMeetup.js

--- apiMeetup.js
+++ apiMeetup.js
@@ -1,12 +1,15 @@
 /***************Fecth data using mongodb where its is realted to model***********/
 var express = require('express'),
      mongoose =require('mongoose'),
+     http = require('http'),
+     waterfall= require('async-waterfall'),
+     async= require('async'),
      bodyParser = require('body-parser');
 
 var db = mongoose.connect('mongodb://localhost/acadglid');
 
 var SignUp   = require('./models/signupMeetModel');
-var Booking = require('./models/putBookingModel');
+var Booking = require('./models/bookingModel');
 var Categories = require('./models/meetUpCategoriesModel');
 
 var app = express();
@@ -20,8 +23,9 @@ var commanRouter = express.Router();
 
 commanRouter.route('/signups')
     .post(function(req,res){
-    	var signup = new SignUp(req.body);
-    	signup.save();
+      var signup = new SignUp(req.body);
+      signup.save();
+      res.send();
 
 
    });
@@ -30,6 +34,7 @@ commanRouter.route('/booking')
     .post(function(req,res){
       var booking = new Booking(req.body);
       booking.save();
+      res.send();
 
 
    });
@@ -44,21 +49,33 @@ commanRouter.route('/getMeetUpCategories')
     })
 });
 
-commanRouter.route('/signin')
-    .get(function(req,res,next){
-    var query ={};
-          var query = {};
-      if(req.query.emailid){
-        query.emailid = req.query.emailid
+commanRouter.route('/userBooked')
+    .get(function(req,res){
+      var query = {};
+      if(req.query.userId){
+        query.userId = req.query.userId
       }
-    SignUp.find(query, function(err,data){
+     var meetId ;
+     Booking.find(query, function(err,data){
       if(err)
-         res.status(500).send()
+         res.status(500).send(err);
       else
-        res.json({"List":data});
-    })
+         meetId = data
+         for (i= 0 ; i<meetId.length; i++){
+             var searchId = (meetId[i].meetingId);
+               console.log(searchId);
+                        Categories.find({"meetUpId":searchId}, function (err, datas) {
+                          if (err) 
+                              res.status(500).send(err);
 
-});
+                          //console.log('%s %s is a %s.', datas)
+                          res.json({"List":datas});
+// Space Ghost is a talk show host.
+})
+        
+        }
+    })
+  })
 
 
 
@@ -67,7 +84,7 @@ app.use('/api', commanRouter);
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req,res){
-	res.send('welcome to api');
+  res.send('welcome to api');
 });
 
 app.get('/image', function(req,res){
@@ -75,5 +92,5 @@ app.get('/image', function(req,res){
 });
 
 app.listen(port, function(){
-	console.log("running");
+  console.log("running");
 });
